97b0ca4aeb44053acff99094af484bb7e962ab89
taylortreece <tayloratreece@gmail.com>
2021-12-30 11:16:10 -0600

    refreshing frontend

--- timer-backend/config/routes.rb
+++ timer-backend/config/routes.rb
@@ -3,3 +3,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root "users#index"
 end
+
+# check routes: http://localhost:3000/rails/info/routes
