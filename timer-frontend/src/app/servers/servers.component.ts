2a7e3624121d5a5aa706ab051b75150e6554654e
taylortreece <tayloratreece@gmail.com>
2021-12-31 15:32:54 -0600

    testing data binding strategies in servers component.

---
+++ timer-frontend/src/app/servers/servers.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-servers',
+  templateUrl: './servers.component.html',
+  styleUrls: ['./servers.component.css']
+})
+export class ServersComponent implements OnInit {
+  allowNewServer = false;
+  serverCreationStatus = "No server was created";
+  serverName = "Testserver";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 3000)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onCreateServer() {
+    this.serverCreationStatus = "Server was created"
+  }
+
+  onUpdateServerName(event: any) {
+    this.serverName = (<HTMLInputElement>event.target).value;
+  }
+}
